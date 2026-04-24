c69dc081a9d1065a6988ed884f81bb0d1ffd9c68
okoyorin-cell <okoyorin-cell@users.noreply.github.com>
2026-04-24 14:49:22 +0100

    sync(cursor): 2026-04-24 13:49:22

--- reborn-test-bot/src/commands/admin-creer-guilde.js
+++ reborn-test-bot/src/commands/admin-creer-guilde.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
 const pg = require('../services/playerGuilds');
 
 module.exports = {
