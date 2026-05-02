8ac01445b848de907b72045c3cc7dc0f6710bfc1
okoyorin-cell <okoyorin-cell@users.noreply.github.com>
2026-05-02 20:13:27 +0100

    sync(cursor): 2026-05-02 19:13:27

--- reborn-test-bot/src/services/earn.js
+++ reborn-test-bot/src/services/earn.js
@@ -9,6 +9,8 @@ const trophies = require('./trophies');
 const rankedRp = require('./rankedRp');
 const skillTree = require('./skillTree');
 const meta = require('./meta');
+const rankedRoles = require('./rankedRoles');
+const indexRoles = require('./indexRoles');
 const { notifyQuestUnlocks } = require('../lib/questNotify');
 
 /** @type {Map<string, { guildId: string, since: number }>} */
