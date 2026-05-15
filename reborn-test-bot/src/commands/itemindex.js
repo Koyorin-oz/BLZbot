92d87460dc8e9a0defdddeccaef4e5bdfe2be5f9
okoyorin-cell <okoyorin-cell@users.noreply.github.com>
2026-05-15 22:50:16 +0100

    sync(cursor): 2026-05-15 21:50:16

--- reborn-test-bot/src/commands/itemindex.js
+++ reborn-test-bot/src/commands/itemindex.js
@@ -3,6 +3,10 @@ const {
   EmbedBuilder,
   PermissionFlagsBits,
   AttachmentBuilder,
+  ContainerBuilder,
+  MediaGalleryBuilder,
+  TextDisplayBuilder,
+  MessageFlags,
 } = require('discord.js');
 const idx = require('../services/indexProgress');
 const users = require('../services/users');
