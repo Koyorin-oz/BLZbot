c2b9cbd0c5cd54d962ed686d4752fb2b38c1ae6f
imroxxor <roxxordev@gmail.com>
2026-05-18 07:00:57 +0200

    Amélioration de la journalisation des messages supprimés : simplification de la gestion du contenu partiel des embeds et des messages

--- modération/src/events/loggingEvents.js
+++ modération/src/events/loggingEvents.js
@@ -266,24 +266,7 @@ module.exports = {
 
         // Relogger la suppression du log (seulement si c'était un message de log)
         if (isOwnMessage) {
-
-            const deletedEmbed = message.embeds[0];
-
-            let partialContent = '**\`Embed sans contenu\`**';
-
-            if (deletedEmbed) {
-                partialContent = [
-                    deletedEmbed.title,
-                    deletedEmbed.description,
-                    ...(deletedEmbed.fields?.map(f => `${f.name}: ${f.value}`) || [])
-                ]
-                    .filter(Boolean)
-                    .join('\n');
-            } else if (message.content) {
-                partialContent = message.content.substring(0, 1000);
-            }
-
-            await logger.log(
+            await this.logger.log(
                 message.guild,
                 '🚨 SÉCURITÉ : Log Supprimé',
                 `Un message de log a été supprimé !`,
@@ -291,7 +274,7 @@ module.exports = {
                 [
                     { name: 'Supprimé par', value: executorName },
                     { name: 'ID de l\'exécuteur', value: executorId },
-                    { name: 'Contenu (partiel)', value: partialContent }
+                    { name: 'Contenu (partiel)', value: (message.embeds[0]?.title || message.content || '[Embed sans titre]').substring(0, 200) }
                 ]
             );
         }
