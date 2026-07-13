17a5667e80f10a015f982e56183aa0e949076b57
imroxxor <roxxordev@gmail.com>
2026-07-13 21:31:12 +0200

    fix: corriger la récupération du nom d'utilisateur réel des développeurs dans la commande de crédits

--- niveau/src/commands/core/credit.js
+++ niveau/src/commands/core/credit.js
@@ -34,7 +34,7 @@ module.exports = {
                 const dev = developers.find(d => d.id === devId);
                 const user = await interaction.client.users.fetch(dev.id);
                 const avatar = user.displayAvatarURL({ size: 256, extension: 'png' });
-                const realName = user.find(d => d.id === dev.id)?.username || dev.name;
+                const realName = user.username || dev.name;
 
                 const container = new ContainerBuilder()
                     .setAccentColor(BLZ_EMBED_STRIP_INT)
